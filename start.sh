00769064b5c4066e41170faf9a4253bf3ce75e41
Varshaa-08 <sjvarshaa8@gmail.com>
2025-04-02 13:39:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@
 # Start FastAPI backend
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:8000 &
 
-# Start Streamlit frontend
-streamlit run app.py --server.port=8501 --server.address=0.0.0.0
+

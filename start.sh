5288978b64b2a0b1ed3dc1b056ed3f321001c344
Varshaa-08 <sjvarshaa8@gmail.com>
2025-04-02 12:00:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Stop any existing instances of FastAPI and Streamlit
-pkill -f "uvicorn backend:app"
-pkill -f "streamlit run app.py"
-
 # Start FastAPI backend
-uvicorn backend:app --host 0.0.0.0 --port 10000 &
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:8000 &
 
 # Start Streamlit frontend
-streamlit run app.py --server.port 8501 --server.address 0.0.0.0
+streamlit run app.py --server.port=8501 --server.address=0.0.0.0

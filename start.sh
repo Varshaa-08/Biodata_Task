ca03ae732bd2358fe0f68ae9c168c572a2fdc614
Varshaa-08 <sjvarshaa8@gmail.com>
2025-04-02 17:36:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Start FastAPI backend
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:8000 &
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker backend:app --bind 0.0.0.0:8000 
 
 
